7c89e0bebf81c10ca32f46897c69e5a6e928d974
Giacomello Nathan <0ddlyokoOfficial@gmail.com>
2020-06-27 23:04:55 +0200

    Change jupiter to junit

--- src/test/java/com/froxynetwork/froxyapi/language/LanguagesTest.java
+++ src/test/java/com/froxynetwork/froxyapi/language/LanguagesTest.java
@@ -1,6 +1,6 @@
 package com.froxynetwork.froxyapi.language;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
 import org.junit.Test;
